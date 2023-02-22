5c592158ad29609fc9d0f01b265108ad83d8d957
phihash <insharp0220@gmail.com>
2023-02-23 02:26:29 +0900

    FizzBuzz完了

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,3 +1,12 @@
 20.times do |i|
-  puts i
+  case
+    when (i + 1) % 15 == 0
+    puts "FizzBuzz"
+    when (i + 1) % 3 == 0
+    puts "Fizz"
+    when (i + 1) % 5 == 0
+    puts "Buzz"
+    else
+    puts i + 1
+    end
 end
